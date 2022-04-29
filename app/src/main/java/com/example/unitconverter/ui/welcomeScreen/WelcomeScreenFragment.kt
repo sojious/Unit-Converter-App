f7e8b3246e4d572cd0157fd62748e09a8e6dc645
Khalid Isah <khalidhulabaloo9@gmail.com>
2022-04-29 22:50:04 +0100

    finished the welcome screen

--- app/src/main/java/com/example/unitconverter/ui/welcomeScreen/WelcomeScreenFragment.kt
+++ app/src/main/java/com/example/unitconverter/ui/welcomeScreen/WelcomeScreenFragment.kt
@@ -2,6 +2,7 @@ package com.example.unitconverter.ui.welcomeScreen
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -10,8 +11,10 @@ import com.example.unitconverter.adapters.WelcomeScreenConversionsAdapter
 import com.example.unitconverter.data.ConversionData
 import com.example.unitconverter.databinding.FragmentWelcomeScreenBinding
 import com.example.unitconverter.model.ConversionItem
+import com.example.unitconverter.util.WelcomeScreenConversionInterface
 
-class WelcomeScreenFragment : Fragment(R.layout.fragment_welcome_screen) {
+class WelcomeScreenFragment : Fragment(R.layout.fragment_welcome_screen),
+WelcomeScreenConversionInterface{
     private lateinit var binding: FragmentWelcomeScreenBinding
     private lateinit var conversionAdapter: WelcomeScreenConversionsAdapter
     private lateinit var items: List<ConversionItem>
@@ -25,10 +28,14 @@ class WelcomeScreenFragment : Fragment(R.layout.fragment_welcome_screen) {
 
     private fun loadConversionData() {
         items = ConversionData.items
-        conversionAdapter = WelcomeScreenConversionsAdapter()
+        conversionAdapter = WelcomeScreenConversionsAdapter(this)
         conversionAdapter.submitList(items)
         binding.WelcomeScreenRecyclerView.adapter = conversionAdapter
         binding.WelcomeScreenRecyclerView.layoutManager =
             GridLayoutManager(activity, 3)
+    }
+
+    override fun goToConversionScreen(id: Int) {
+        Toast.makeText(activity, "You clicked on $id", Toast.LENGTH_SHORT).show()
     }
 }
