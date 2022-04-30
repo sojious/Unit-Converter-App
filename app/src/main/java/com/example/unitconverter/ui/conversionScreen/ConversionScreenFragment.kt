7918d57f181725128073633e1120524b1add504f
precious3173 <66275437+precious3173@users.noreply.github.com>
2022-04-30 09:51:25 +0100

    New update on conversion fragment xml

--- app/src/main/java/com/example/unitconverter/ui/conversionScreen/ConversionScreenFragment.kt
+++ app/src/main/java/com/example/unitconverter/ui/conversionScreen/ConversionScreenFragment.kt
@@ -1,6 +1,18 @@
 package com.example.unitconverter.ui.conversionScreen
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.example.unitconverter.R
+import com.example.unitconverter.databinding.FragmentConversionScreenBinding
 
-class ConversionScreenFragment : Fragment(){
+class ConversionScreenFragment : Fragment(R.layout.fragment_conversion_screen){
+
+    private lateinit var binding: FragmentConversionScreenBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+   binding = FragmentConversionScreenBinding.bind(view)
+
+    }
 }
